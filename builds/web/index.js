fe37298ebe2febbb290ab64c3cadc4577e35dca9
JackOatley <jack.f.oatley@gmail.com>
2021-04-23 16:22:50 +0100

    Updated build

--- builds/web/index.js
+++ builds/web/index.js
@@ -15,9 +15,11 @@ canvas.addEventListener("contextmenu", e => {
 
 // Set various properties of the window.
 function setMode(opts) {
-	document.title = opts.name || document.title;
-	canvas.width = opts.width || canvas.width;
-	canvas.height = opts.height || canvas.height;
+	document.title = opts.name ?? document.title;
+	canvas.width = opts.resWidth ?? canvas.width;
+	canvas.height = opts.resHeight ?? canvas.height;
+	canvas.style.width = opts.width ?? canvas.width;
+	canvas.style.height = opts.height ?? canvas.height;
 }
 
 //
@@ -46,9 +48,10 @@ function newSound(src, n=1) {
 
 // Plays a given sound. The function loops through all instances of the sound to find one that isn't playing, and plays it.
 function play(sound) {
-	for (let n = 0; n < 11; n++) {
+	for (let n = 0; n < sound.instances.length; n++) {
 		if (sound.instances[n].paused) {
-			return sound.instances[n].play();
+			const i = sound.instances[n].play();
+			return i;
 		}
 	}
 	return null;
@@ -84,11 +87,22 @@ function clear(r=0, g=0, b=0, a=1) {
 }
 
 //
-function draw(i, x, y, r=0, sx=1, sy=1) {
-	target.drawImage(i,
+function draw$1(i, x, y, r=0, sx=1, sy=1) {
+
+	// Draw image.
+	if (i instanceof Image)
+		return target.drawImage(i,
+			x - i.ox * sx, y - i.oy * sy,
+			i.width * sx, i.height * sy
+		);
+
+	// Draw subimage.
+	target.drawImage(i.img,
+		i.x, i.y, i.w, i.h,
 		x - i.ox * sx, y - i.oy * sy,
-		i.width * sx, i.height * sy
+		i.w * sx, i.h * sy
 	);
+
 }
 
 // Draw a line from a variable number of coordinates.
@@ -110,6 +124,16 @@ function newImage(url, ox=0, oy=0) {
 	img.ox = ox;
 	img.oy = oy;
 	return img;
+}
+
+//
+function newSubImage(img, x, y, w, h, ox=0, oy=0) {
+	return {
+		img: img,
+		x: x, y: y,
+		w: w, h: h,
+		ox: ox, oy: oy
+	}
 }
 
 //
@@ -135,7 +159,7 @@ function rectangle(x, y, w, h) {
 }
 
 // Set the drawing color.
-function setColor(r, g, b, a) {
+function setColor(r, g, b, a=1) {
 	target.strokeStyle = target.fillStyle = `rgba(${r},${g},${b},${a})`;
 }
 
@@ -161,9 +185,10 @@ function setTextBaseline(x) {
 var graphics = {
 	circle: circle,
 	clear: clear,
-	draw: draw,
+	draw: draw$1,
 	line: line,
 	newImage: newImage,
+	newSubImage: newSubImage,
 	points: points,
 	print: print,
 	rectangle: rectangle,
@@ -173,57 +198,63 @@ var graphics = {
 	setTextBaseline: setTextBaseline
 };
 
-const _pressed = {};
-const _released = {};
-const _down = {};
+const _pressed$1 = {};
+const _released$1 = {};
+const _down$1 = {};
 
 document.addEventListener("keydown", (e) => {
 	if (e.repeat) return;
-	_pressed[e.code] = true;
-	_released[e.code] = false;
-	_down[e.code] = true;
+	_pressed$1[e.code] = true;
+	_released$1[e.code] = false;
+	_down$1[e.code] = true;
 });
 
 document.addEventListener("keyup", (e) => {
-	_pressed[e.code] = false;
-	_released[e.code] = true;
-	_down[e.code] = false;
+	_pressed$1[e.code] = false;
+	_released$1[e.code] = true;
+	_down$1[e.code] = false;
 });
 
 // Returns true if the given key is currently held down.
-function down(code) {
-	return _down[code] === true;
+function down$1(code) {
+	return _down$1[code] === true;
 }
 
 // Returns true if the given key was pressed in the current tick.
-function pressed(code) {
-	return _pressed[code] === true;
+function pressed$1(code) {
+	return _pressed$1[code] === true;
 }
 
 // Returns true if the given key was released in the current tick.
-function released(code) {
-	return _released[code] === true;
+function released$1(code) {
+	return _released$1[code] === true;
 }
 
 //
-function update() {
-	for (const key in _down) {
-		_pressed[key] = false;
-		_released[key] = false;
+function update$2() {
+	for (const key in _down$1) {
+		_pressed$1[key] = false;
+		_released$1[key] = false;
 	}
 }
 
 //
 var keyboard = {
-	down: down,
-	pressed: pressed,
-	released: released,
-	update: update
+	down: down$1,
+	pressed: pressed$1,
+	released: released$1,
+	update: update$2
 };
 
 //
 function choose(...args) {
 	return args[~~random(args.length)];
+}
+
+//
+function distance(x1, y1, x2, y2) {
+	const x = x1 - x2, y = y1 - y2;
+	return Math.sqrt(x * x + y * y);
 }
 
 //
@@ -238,12 +269,13 @@ function random(x, y) {
 //
 var math = {
 	choose: choose,
+	distance: distance,
 	random: random
 };
 
-const _down$1 = new Array(3);
-const _pressed$1 = new Array(3);
-const _released$1 = new Array(3);
+const _down = new Array(3);
+const _pressed = new Array(3);
+const _released = new Array(3);
 
 let x = 0;
 let y = 0;
@@ -258,15 +290,15 @@ document.body.addEventListener("mousemove", e => {
 });
 
 document.body.addEventListener("mousedown", e => {
-	_down$1[e.button] = true;
-	_pressed$1[e.button] = true;
-	_released$1[e.button] = false;
+	_down[e.button] = true;
+	_pressed[e.button] = true;
+	_released[e.button] = false;
 });
 
 document.body.addEventListener("mouseup", e => {
-	_down$1[e.button] = false;
-	_pressed$1[e.button] = false;
-	_released$1[e.button] = true;
+	_down[e.button] = false;
+	_pressed[e.button] = false;
+	_released[e.button] = true;
 });
 
 // Returns the current position of the mouse.
@@ -275,18 +307,18 @@ function position() {
 }
 
 // Returns true if the given button is currently held down.
-function down$1(button) {
-	return _down$1[button] === true;
+function down(button) {
+	return _down[button] === true;
 }
 
 // Returns true if the given button was pressed in the current tick.
-function pressed$1(button) {
-	return _pressed$1[button] === true;
+function pressed(button) {
+	return _pressed[button] === true;
 }
 
 // Returns true if the given button was released in the current tick.
-function released$1(button) {
-	return _released$1[button] === true;
+function released(button) {
+	return _released[button] === true;
 }
 
 // Update the mouse states, this is done automatically in engine.js.
@@ -294,16 +326,16 @@ function update$1() {
 	moveX = 0;
 	moveY = 0;
 	for (let n = 0; n < 3; n++) {
-		_pressed$1[n] = _released$1[n] = false;
+		_pressed[n] = _released[n] = false;
 	}
 }
 
 //
 var mouse = {
 	position: position,
-	down: down$1,
-	pressed: pressed$1,
-	released: released$1,
+	down: down,
+	pressed: pressed,
+	released: released,
 	update: update$1
 };
 
@@ -311,77 +343,61 @@ let updateCallback;
 let drawCallback;
 
 // Sets the update function.
-function update$2(callback) {
+function update(callback) {
 	updateCallback = callback;
+}
+
+// Sets the draw function.
+function draw(callback) {
+	drawCallback = callback;
 }
 
 // Executes the update function of an object or for each object in an array.
 function updateList(x) {
 	if (x.paused === false) return;
-	if (Array.isArray(x))
-		x.forEach(updateList);
-	else
-		x.update && x.update();
+	if (!Array.isArray(x)) return x.update && x.update();
+	x.forEach(updateList);
 }
 
 // Executes the draw function of an object or for each object in an array.
 function drawList(x) {
 	if (x.visible === false) return;
-	if (Array.isArray(x))
-		x.forEach(drawList);
-	else
-		x.draw && x.draw();
+	if (!Array.isArray(x)) return x.draw && x.draw();
+	x.forEach(drawList);
 }
 
-// Sets the draw function.
-function draw$1(callback) {
-	drawCallback = callback;
-}
-
-// Setup main loop.
-function mainLoop(time) {
+// Main loop.
+(function mainLoop(time) {
 	updateCallback && updateCallback();
 	drawCallback && drawCallback();
 	keyboard.update();
 	mouse.update();
 	requestAnimationFrame(mainLoop);
-}
-
-// Start main loop.
-mainLoop();
+})();
 
 //
 var game = {
 	app: app,
 	audio: audio,
-	draw: draw$1,
+	draw: draw,
 	drawList: drawList,
 	graphics: graphics,
 	keyboard: keyboard,
 	math: math,
 	mouse: mouse,
-	update: update$2,
+	update: update,
 	updateList: updateList
 };
 
-game.app.setMode({
-	name: "Jamulator 2021",
-	width: 640,
-	height: 480
-});
+//
+class GameObject {
 
-const smiley = game.graphics.newImage("art/smiley.png", 729/2, 729/2);
-
-class Shape {
-
-	constructor(x, y) {
-		this.x = x || game.math.random(640);
-		this.y = y || game.math.random(480);
-		this.vx = game.math.random(-5, 5);
-		this.vy = game.math.random(-5, 5);
-		this.r = game.math.random(255);
-		this.g = game.math.random(255);
-		this.b = game.math.random(255);
+	constructor(img, x=0, y=0) {
+		this.img = img;
+		this.x = x;
+		this.y = y;
+		this.vx = 0;
+		this.vy = 0;
 	}
 
 	update(dt) {
@@ -392,70 +408,113 @@ class Shape {
 	}
 
 	draw() {
-		game.graphics.setColor(this.r, this.g, this.b, 1);
+		this.img && game.graphics.draw(this.img, this.x, this.y);
 	}
 
 }
 
-class Circle extends Shape {
+const sndPlayerMove = game.audio.newSound("audio/playerMove.wav", 2);
 
-	constructor(x, y) {
-		super(x, y);
-		this.radius = game.math.random(20, 70);
+//
+const playerDog = new GameObject(null, 64, 180-16);
+
+playerDog.moveToX = playerDog.x;
+playerDog.moveToY = playerDog.y;
+playerDog.nextMove = {};
+
+//
+playerDog.update = function() {
+
+	let next = this;
+
+	if (this.moveToX !== this.x || this.moveToY !== this.y) {
+		this.x += Math.sign(this.moveToX - this.x);
+		this.y += Math.sign(this.moveToY - this.y);
+		next = this.nextMove;
+		if (game.math.distance(this.x, this.y, this.moveToX, this.moveToY) === 15)
+			game.audio.play(sndPlayerMove);
 	}
 
-	draw() {
-		super.draw();
-		game.graphics.circle(this.x, this.y, this.radius);
+	else {
+		if (this.nextMove.moveToX !== undefined
+		||  this.nextMove.moveToY !== undefined) {
+			this.moveToX = this.nextMove.moveToX ?? this.x;
+			this.moveToY = this.nextMove.moveToY ?? this.y;
+			this.nextMove = {};
+		}
 	}
 
+	const nowX = this.moveToX;
+	const nowY = this.moveToY;
+
+	if (game.keyboard.pressed("ArrowUp")) {
+		next.moveToX = nowX;
+		next.moveToY = nowY - 16;
+	}
+
+	if (game.keyboard.pressed("ArrowDown")) {
+		next.moveToX = nowX;
+		next.moveToY = nowY + 16;
+	}
+
+	if (game.keyboard.pressed("ArrowLeft")) {
+		next.moveToX = nowX - 16;
+		next.moveToY = nowY;
+	}
+
+	if (game.keyboard.pressed("ArrowRight")) {
+		next.moveToX = nowX + 16;
+		next.moveToY = nowY;
+	}
+
+};
+
+//
+playerDog.draw = function() {
+	game.graphics.setColor(255, 0, 0);
+	game.graphics.rectangle(this.x+2, this.y+2, 12, 12);
+};
+
+game.app.setMode({
+	name: "Jamulator 2021",
+	width: 320 * 4,
+	height: 180 * 4,
+	resWidth: 320,
+	resHeight: 180
+});
+
+const tiles = game.graphics.newImage("art/tiles.png");
+const grass = game.graphics.newSubImage(tiles, 0, 0, 16, 16);
+const grassEdge = game.graphics.newSubImage(tiles, 32, 0, 16, 16);
+const road = game.graphics.newSubImage(tiles, 0, 16, 16, 16);
+const roadDash = game.graphics.newSubImage(tiles, 16, 16, 16, 16, 8, 0);
+
+const gameObjects = [];
+
+// Generate map.
+const order = [grass, road, road, grass, road, road, grass, grass, road, road, grass];
+for (let y = 180 - 16, n = 0; y >= 0; y -= 16, n++) {
+	for (let x = 0; x < 640; x += 16) {
+
+		if (order[n] === grass && order[n-1] === road)
+			gameObjects.push(new GameObject(grassEdge, x, y));
+
+		else
+			gameObjects.push(new GameObject(order[n], x, y));
+
+		if (order[n] === road && order[n-1] === road && (x % 32 === 0))
+			gameObjects.push(new GameObject(roadDash, x, y + 8));
+
+	}
 }
 
-class Rectangle extends Shape {
-
-	constructor(x, y) {
-		super(x, y);
-		this.size = game.math.random(20, 70);
-	}
-
-	draw() {
-		super.draw();
-		game.graphics.rectangle(this.x, this.y, this.size, this.size);
-	}
-
-}
-
-class Smiley extends Shape {
-
-	constructor(x, y) {
-		super(x, y);
-		this.scale = game.math.random(0.1, 0.2);
-	}
-
-	draw() {
-		super.draw();
-		game.graphics.draw(smiley, this.x, this.y, 0, this.scale, this.scale);
-	}
-
-}
-
-const circles = [];
+gameObjects.push(playerDog);
 
 game.update((dt) => {
-	if (game.keyboard.pressed("Space")) {
-		const c = game.math.choose(Circle, Rectangle, Smiley);
-		circles.push(new c(...game.mouse.position()));
-	}
-	game.updateList(circles);
+	game.updateList(gameObjects);
 });
 
 game.draw(() => {
 	game.graphics.clear(0, 0, 0);
-	game.drawList(circles);
-	game.graphics.setColor(255, 255, 255, 1);
-	game.graphics.line(...circles.flatMap(i => [i.x, i.y]));
-	game.graphics.setTextAlign("center");
-	game.graphics.setTextBaseline("middle");
-	game.graphics.setFont("italic small-caps bold 64px cursive");
-	game.graphics.print("Hello, world!", 320, 240);
+	game.drawList(gameObjects);
 });
