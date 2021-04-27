23deca09732ad3af0852e0657f0755a927955d9c
JackOatley <jack.f.oatley@gmail.com>
2021-04-27 18:16:01 +0100

    audio.play can take an array of sounds and play one randomly

--- src/engine/audio.js
+++ src/engine/audio.js
@@ -1,3 +1,4 @@
+import { choose } from "./math.js";
 
 // Create a new sound from the given src (url to file). "n" let's us specify how many instances of this sound should be generated, if we need to play multiple of the same sound at once.
 export function newSound(src, n=1) {
@@ -18,6 +19,7 @@ export function newSound(src, n=1) {
 
 // Plays a given sound. The function loops through all instances of the sound to find one that isn't playing, and plays it.
 export function play(sound) {
+	if (Array.isArray(sound)) return play(choose(...sound));
 	for (let n = 0; n < sound.instances.length; n++) {
 		if (sound.instances[n].paused) {
 			return sound.instances[n].play();
